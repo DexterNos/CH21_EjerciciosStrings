654537ea8d19b642c3aa4148a78abfd1546b6b80
Jonathan Bautista <jon.bau.aco@outlook.com>
2022-12-12 12:47:00 -0700

    Primer ejercicio Strings

--- js/main.js
+++ js/main.js
@@ -1,1 +1,40 @@
 //Ejercicios Strings
+let nombre = "Jonathan";
+let letra ="";
+let contFinal=0;
+
+for (let index = 0; index < nombre.length; index++) {
+    console.log(nombre.charAt(index));
+    let contTemp = 1;
+    for (let cont = index+1; cont < nombre.length; cont++){
+        if(nombre.charAt(index)==nombre.charAt(cont)){
+            contTemp++;
+        }
+    }
+    if (contTemp>contFinal) {
+        contFinal=contTemp;
+        letra = nombre.charAt(index);
+        
+    }
+    console.log(letra, contFinal);
+    
+}
+
+/*function charRep(string) {
+    letra = string.toLowerCase();
+    for (let index = 0; index < letra.length; index++) {
+        const element = letra[index];
+        //console.log(element);
+
+        let contador;
+        contador = 0;
+        if (letra.charAt() == letra[index]) {
+            contador++;
+            console.log(letra[index] + " " + contador);
+        }
+        
+    
+    }
+    
+}*/
+
